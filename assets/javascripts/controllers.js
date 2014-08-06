05c297e48dffd6f20c57a6b5fdf53ec394c10129
Lucas Videla <videla.lucas@gmail.com>
2014-08-05 23:56:11 -0300

    Improved UI and automatic refresh of posts (polling)

--- assets/javascripts/controllers.js
+++ assets/javascripts/controllers.js
@@ -3,9 +3,19 @@ var broadcastorApp = angular.module('broadcastorApp', ['angularMoment'])
   return function(unixDate) {
     return moment(unixDate * 1000).fromNow();
   }
+})
+.filter('pluralize', function() {
+  return function(length) {
+    return length > 1 ? 's' : '';
+  }
 });
 
 broadcastorApp.controller('ChannelCtrl', function ($scope, $http) {
+
+  var interval = 5000;
+
+  $scope.posts = []
+  $scope.newPosts = [];
 
   $http.get('/channels/' + channel.slug + '.json').
     success(function(data, status, headers, config) {
@@ -15,18 +25,20 @@ broadcastorApp.controller('ChannelCtrl', function ($scope, $http) {
       console.log('Error loading initial posts');
     });
 
-  $scope.newPosts = [];
-
   $scope.peek = function () {
     var timestamp = $scope.newPosts.length > 0 ? $scope.newPosts[0].timestamp :
       $scope.posts.length > 0 ? $scope.posts[0].timestamp : 0;
     $http.get('/channels/' + channel.slug + '/after/' + timestamp + '.json')
       .success(function(data, status, headers, config) {
+        interval = 5000;
         if (data) $scope.newPosts = data.concat($scope.newPosts);
       })
       .error(function(data, status, headers, config) {
+        interval = interval * 2;
         console.log('Error updating posts');
       });
+
+    window.setTimeout($scope.peek, interval);
   };
 
   $scope.update = function() {
@@ -34,8 +46,6 @@ broadcastorApp.controller('ChannelCtrl', function ($scope, $http) {
     $scope.newPosts = [];
   }
 
-  setInterval(function(){
-    $scope.peek()
-  },5000);
+  window.setTimeout($scope.peek, interval);
 
 });
